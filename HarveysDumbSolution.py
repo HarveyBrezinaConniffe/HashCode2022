0bf569b9ea8f08a0960c813a6f9ed99b73dbc301
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-02-24 18:23:26 +0000

    Added project parsing.

--- HarveysDumbSolution.py
+++ HarveysDumbSolution.py
@@ -4,8 +4,10 @@ numContributors, numProjects = map(int, input("").split(" "))
 
 Skill = namedtuple("Skill", ["name", "level"])
 Person = namedtuple("Person", ["name", "skills"])
+Project = namedtuple("Project", ["name", "days", "score", "bestBefore", "roles"])
 
 people = []
+projects = []
 
 for i in range(numContributors):
 	personName, numSkills = input("").split()
@@ -15,4 +17,18 @@ for i in range(numContributors):
 		skills.append(Skill(skillName, int(level)))
 	people.append(Person(personName, skills))
 
+for i in range(numProjects):
+	projectName, days, score, bestBefore, numRoles = input().split(" ")
+	days = int(days)
+	score = int(score)
+	bestBefore = int(bestBefore)
+	numRoles = int(numRoles)
+	roles = []
+	for j in range(numRoles):
+		skillName, level = input("").split()
+		roles.append(Skill(skillName, int(level)))
+	projects.append(Project(projectName, days, score, bestBefore, roles))
+
 print(people)
+print()
+print(projects)
