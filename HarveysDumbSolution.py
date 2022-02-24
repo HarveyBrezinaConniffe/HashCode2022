d9713a3b92b9db15c2f642e9723c656771ce789f
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-02-24 18:09:54 +0000

    Now parsing people.

--- HarveysDumbSolution.py
+++ HarveysDumbSolution.py
@@ -1,2 +1,18 @@
+from collections import namedtuple
+
 numContributors, numProjects = map(int, input("").split(" "))
-print(numContributors, numProjects, type(numContributors))
+
+Skill = namedtuple("Skill", ["name", "level"])
+Person = namedtuple("Person", ["name", "skills"])
+
+people = []
+
+for i in range(numContributors):
+	personName, numSkills = input("").split()
+	skills = []
+	for j in range(int(numSkills)):
+		skillName, level = input("").split()
+		skills.append(Skill(skillName, int(level)))
+	people.append(Person(personName, skills))
+
+print(people)
